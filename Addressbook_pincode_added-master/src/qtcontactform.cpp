23fa21c33e78d156c07263aba0d1f7ef7398548c
karthik55555 <karthikdivs7@ymail.com>
2016-12-23 10:46:17 +0530

    Add files via upload

---
+++ Addressbook_pincode_added-master/src/qtcontactform.cpp
@@ -0,0 +1,32 @@
+#include <QLabel>
+#include <QLineEdit>
+#include <QFormLayout>
+#include "qtcontactform.h"
+
+
+
+QtContactForm::QtContactForm(QWidget *parent) : 
+        QFormLayout(parent), firstNameField(new QLineEdit()),
+        lastNameField(new QLineEdit()),
+        addressField(new QLineEdit()),
+        pincodeField(new QLineEdit()),
+        phoneNumberField(new QLineEdit()),
+        emailField(new QLineEdit())
+{
+    addRow("<font color ='blue'>First Name</font>", firstNameField);
+    addRow("<font color ='blue'>Last Name</font>", lastNameField);
+    addRow("<font color ='blue'>Address</font>", addressField);
+     addRow("<font color ='blue'>Pincode</font>", pincodeField);
+    addRow("<font color ='blue'>Phone Number</font>", phoneNumberField);
+    addRow("<font color ='blue'>Email</font>", emailField);
+}
+
+void QtContactForm::clear()
+{
+    firstNameField->setText("");
+    lastNameField->setText("");
+    addressField->setText("");
+    pincodeField->setText("");
+    phoneNumberField->setText("");
+    emailField->setText("");
+}
